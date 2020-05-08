3d51d62db2ca2325c8584c66cb98aa34de4b1309
siilavanta <53584681+siilavanta@users.noreply.github.com>
2020-05-08 16:44:49 +0600

    Create splite_word.js

---
+++ splite_word.js
@@ -0,0 +1,8 @@
+p = '<strong>word</strong>: or <em>word</em> or <p><strong>word</strong>: this is a sentence</p>'
+
+var tempDivElement = document.createElement("div");
+tempDivElement.innerHTML = p;
+
+let t = tempDivElement.innerText
+let words = t.match(/\w+|\S/g)
+console.log(words)
