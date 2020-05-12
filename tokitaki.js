5fd0e2ab6b4cc56716f85b8bf0b9c7ce8954b7e1
siilavanta <53584681+siilavanta@users.noreply.github.com>
2020-05-12 09:14:22 +0600

    Update tokitaki.js

--- tokitaki.js
+++ tokitaki.js
@@ -1,80 +1,80 @@
 
-// function booklabelopen(evt, headactive, query, bookactive){
-//   var pitakaName = document.getElementsByClassName('pitakaName')
-//   for(var i = 0; i < pitakaName.length; i++){
-//     pitakaName[i].className = pitakaName[i].className.replace(`${bookactive}`, '')
-//   }
+function booklabelopen(evt, headactive, query, bookactive){
+  var pitakaName = document.getElementsByClassName('pitakaName')
+  for(var i = 0; i < pitakaName.length; i++){
+    pitakaName[i].className = pitakaName[i].className.replace(`${bookactive}`, '')
+  }
 
-//   //var mulvi = document.querySelectorAll(`${query}`)
-//   // mulvi.classList.toggle('vinayabook')
-//   document.querySelectorAll(`.${query}`).forEach( (li) =>{
-//     li.toggleAttribute('class', `${bookactive}`)
-//   })
-//   evt.currentTarget.className += ` ${headactive}`
+  //var mulvi = document.querySelectorAll(`${query}`)
+  // mulvi.classList.toggle('vinayabook')
+  document.querySelectorAll(`.${query}`).forEach( (li) =>{
+    li.toggleAttribute('class', `${bookactive}`)
+  })
+  evt.currentTarget.className += ` ${headactive}`
 
-// }
-// booklabelopen(event, "pitakaactive",  "mulvi", "vinayactive" )
+}
+booklabelopen(event, "pitakaactive",  "mulvi", "vinayactive" )
 
-// function openCity(evt, cityName) {
-//   var i, tabcontent, pitakaName;
-//   tabcontent = document.getElementsByClassName("tabcontent");
-//   for (i = 0; i < tabcontent.length; i++) {
-//   //tabcontent[i].style.display = "none";
-//   }
-//   pitakaName = document.getElementsByClassName("pitakaName");
-//   for (i = 0; i < pitakaName.length; i++) {
-//   pitakaName[i].className = pitakaName[i].className.replace(" active", "");
-//   }
-//   document.getElementById(cityName).classList.toggle("show");
-//   evt.currentTarget.className += " active";
-//   }
+function openCity(evt, cityName) {
+  var i, tabcontent, pitakaName;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+  //tabcontent[i].style.display = "none";
+  }
+  pitakaName = document.getElementsByClassName("pitakaName");
+  for (i = 0; i < pitakaName.length; i++) {
+  pitakaName[i].className = pitakaName[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).classList.toggle("show");
+  evt.currentTarget.className += " active";
+  }
 
-  //   document.getElementById('pitakaName1').onclick = function(){
-  //     var fruits = ["apple", "orange", "cherry"];
-  //     fruits.forEach(myFunction);
+    document.getElementById('pitakaName1').onclick = function(){
+      var fruits = ["apple", "orange", "cherry"];
+      fruits.forEach(myFunction);
 
-  //     function myFunction(item, index) {
-  //     console.log( index + ":" + item ) 
-  //   }
-  // }
-//var x = document.getElementsByTagName("div");
-// console.log(x.item().innerHTML);
-
-
-//  function createbookname(element, id, Class){
-//      var ul = document.createElement('ul')
-//      ul.setAttribute('class', 'someclass')
-
-//  }
-
-// var sutta = document.getElementById('sutta')
-//     var dn = document.getElementById('dn')
-//     var mn = document.getElementById('mn')
-//     var sn = document.getElementById('sn')
-//     var an = document.getElementById('an')
-//     var kn = document.getElementById('kn')
-// var abhi = document.getElementById('abhi')
-
-//var vinaya = document.getElementById('vinaya')
+      function myFunction(item, index) {
+      console.log( index + ":" + item ) 
+    }
+  }
+var x = document.getElementsByTagName("div");
+console.log(x.item().innerHTML);
 
 
-// document.getElementById('addnew').onclick = function(){
-    // var vinaya = document.getElementById('vinaya'); 
-    // var newGroup = document.createElement("div"); 
-    // newGroup.setAttribute("id", "newGroup"); 
-    // newGroup.setAttribute("class", "inner");
-    // parent.appendChild(newGroup); 
-    // console.log(newGroup)
-// };
-//palibooks('muvi', vi, 'mulvin')
+ function createbookname(element, id, Class){
+     var ul = document.createElement('ul')
+     ul.setAttribute('class', 'someclass')
 
-// var ul = document.createElement('ul')
-// ul.setAttribute('id', 'someclass')
-// var note = document.createTextNode('বিনয় পিটক')
-// ul.appendChild(note)
-// vinaya.appendChild(ul)
+ }
 
-// console.log(ul)
-// setTimeout(function(){
+var sutta = document.getElementById('sutta')
+    var dn = document.getElementById('dn')
+    var mn = document.getElementById('mn')
+    var sn = document.getElementById('sn')
+    var an = document.getElementById('an')
+    var kn = document.getElementById('kn')
+var abhi = document.getElementById('abhi')
+
+var vinaya = document.getElementById('vinaya')
+
+
+document.getElementById('addnew').onclick = function(){
+    var vinaya = document.getElementById('vinaya'); 
+    var newGroup = document.createElement("div"); 
+    newGroup.setAttribute("id", "newGroup"); 
+    newGroup.setAttribute("class", "inner");
+    parent.appendChild(newGroup); 
+    console.log(newGroup)
+};
+palibooks('muvi', vi, 'mulvin')
+
+var ul = document.createElement('ul')
+ul.setAttribute('id', 'someclass')
+var note = document.createTextNode('বিনয় পিটক')
+ul.appendChild(note)
+vinaya.appendChild(ul)
+
+console.log(ul)
+setTimeout(function(){
    
-// }, 2000)
+}, 2000)
